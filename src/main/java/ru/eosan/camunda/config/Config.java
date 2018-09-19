c28519b24cb47113fd15d288c62da107dfa9a2fa
Aleksandr Streltsov <jness.pro@ya.ru>
2018-09-19 18:47:42 +0300

    rest api

--- src/main/java/ru/eosan/camunda/config/Config.java
+++ src/main/java/ru/eosan/camunda/config/Config.java
@@ -9,21 +9,6 @@ import javax.servlet.Filter;
 @Configuration
 public class Config {
 
-//    @Bean
-//    public FilterRegistrationBean processEngineAuthenticationFilterRegistration() {
-//        FilterRegistrationBean registration = new FilterRegistrationBean();
-//        registration.setFilter(processEngineAuthenticationFilter());
-//        registration.addUrlPatterns("/*");
-//        registration.addInitParameter("authentication-provider", "org.camunda.bpm.engine.rest.security.auth.impl.HttpBasicAuthenticationProvider");
-//        registration.setName("camunda-auth");
-//        registration.setOrder(1);
-//        return registration;
-//    }
-//
-//    public Filter processEngineAuthenticationFilter() {
-//        return new ProcessEngineAuthenticationFilter();
-//    }
-
     @Bean
     public FilterRegistrationBean autoLoginAuthenticationFilterRegistration() {
         FilterRegistrationBean registration = new FilterRegistrationBean();
